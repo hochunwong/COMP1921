e57384e20fe92a3e53a59f90f565ea443521d78b
Ho Chun Wong <146675110+hochunwong@users.noreply.github.com>
2024-02-28 23:56:18 +0000

    wasd testing

--- testScript9000.sh
+++ testScript9000.sh
@@ -30,9 +30,9 @@ else
 fi
 
 # Test bad argument (missing file)
-echo -e -n "Testing 2 arguments . . . "
+echo -e -n "Testing non existing file . . . "
 ./maze thisFileDoesNOTexist.csv > output.tmp
-if grep -q "Error, file not found." output.tmp;
+if grep -q "Error - file not found." output.tmp;
 then
     echo -e "\e[32mPass :)\e[0m"
 else
@@ -50,9 +50,9 @@ echo -e "Map Display and Player Movement Tests"
 
 # Test Map Print
 echo -e -n "Testing map display . . . "
-./maze data/mazes/valid/wasd-testing/wasd-tester.txt < data/inputs/WASD/map.in
+./maze data/mazes/valid/wasd-testing/wasd-tester.txt < data/inputs/WASD/map.in > output.tmp
 # 2 possibilities - X covers S or X does not cover S
-if [grep -q data/mazes/valid/wasd-testing/wasd-tester.txt output.tmp] || [grep -q data/mazes/valid/wasd-testing/correct-m-ifcoverS.txt output.tmp];
+if [grep -Fxvf data/mazes/valid/wasd-testing/wasd-tester.txt output.tmp] || [grep -Fxvf data/mazes/valid/wasd-testing/correct-m-ifcoverS.txt output.tmp];
 then
     echo -e "\e[32mPass :)\e[0m"
 else
@@ -60,21 +60,42 @@ else
 fi
 
 # Test W
+echo -e -n "Testing player movement (W) . . . "
+./maze data/mazes/valid/wasd-testing/wasd-tester.txt < data/inputs/WASD/W.in > output.tmp
+if grep -Fxvf data/mazes/valid/wasd-testing/correct-w.txt output.tmp;
+then
+    echo -e "\e[32mPass :)\e[0m"
+else
+    echo -e "\e[31mFail ;(\e[0m"
+fi
 
 # Test A
+echo -e -n "Testing player movement (A) . . . "
+./maze data/mazes/valid/wasd-testing/wasd-tester.txt < data/inputs/WASD/A.in > output.tmp
+if grep -Fxvf data/mazes/valid/wasd-testing/correct-a.txt output.tmp;
+then
+    echo -e "\e[32mPass :)\e[0m"
+else
+    echo -e "\e[31mFail ;(\e[0m"
+fi
 
 # Test S
+echo -e -n "Testing player movement (S) . . . "
+./maze data/mazes/valid/wasd-testing/wasd-tester.txt < data/inputs/WASD/S.in > output.tmp
+if grep -Fxvf data/mazes/valid/wasd-testing/correct-s.txt output.tmp;
+then
+    echo -e "\e[32mPass :)\e[0m"
+else
+    echo -e "\e[31mFail ;(\e[0m"
+fi
 
 # Test D
-
-
-# DELETE ME LATER
-
-#    echo -n "Testing x - "
-#    ./maze > tmp
-#    if grep -q "x" tmp;
-#    then
-#        echo -e "\e[32mPass :)\e[0m"
-#    else
-#        echo -e "\e[31mFail ;(\e[0m"
-#    fi
+echo -e -n "Testing player movement (D) . . . "
+./maze data/mazes/valid/wasd-testing/wasd-tester.txt < data/inputs/WASD/D.in > output.tmp
+if grep -Fxvf data/mazes/valid/wasd-testing/correct-d.txt output.tmp;
+then
+    echo -e "\e[32mPass :)\e[0m"
+else
+    echo -e "\e[31mFail ;(\e[0m"
+fi
+rm -f output.tmp
