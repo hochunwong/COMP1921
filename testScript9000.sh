5d5b7466b7d15b7a6f73b13e6b6b711b4b0976b2
Ho Chun Wong <146675110+hochunwong@users.noreply.github.com>
2024-02-29 00:30:25 +0000

    Maze Checks

--- testScript9000.sh
+++ testScript9000.sh
@@ -6,6 +6,7 @@
 
 echo -e "\n\e[1m\e[36m\t~ maze program test script 9000 ~\n\e[0m"
 
+################################################################################
 
 echo -e "Argument Tests"
 
@@ -39,19 +40,98 @@ else
     echo -e "\e[31mFail ;(\e[0m"
 fi
 
+################################################################################
 
 echo -e "\nMaze Checking Tests"
 
-# Test maze dimensions (width)
+# Too few rows
+echo -e -n "Testing too few rows . . . "
+./maze data/mazes/invalid/tooFewRows.txt > output.tmp
+if grep -q "Error - maze too small";
+then
+    echo -e "\e[32mPass :)\e[0m"
+else
+    echo -e "\e[31mFail ;(\e[0m"
+fi
 
+# Too few cols
+echo -e -n "Testing too few cols . . . "
+./maze data/mazes/invalid/tooFewCols.txt > output.tmp
+if grep -q "Error - maze too small";
+then
+    echo -e "\e[32mPass :)\e[0m"
+else
+    echo -e "\e[31mFail ;(\e[0m"
+fi
 
+# Too many rows
+echo -e -n "Testing too many rows . . . "
+./maze data/mazes/invalid/tooManyRows.txt > output.tmp
+if grep -q "Error - maze too big";
+then
+    echo -e "\e[32mPass :)\e[0m"
+else
+    echo -e "\e[31mFail ;(\e[0m"
+fi
+
+# Too many cols
+echo -e -n "Testing too many cols . . . "
+./maze data/mazes/invalid/tooManyCols.txt > output.tmp
+if grep -q "Error - maze too big";
+then
+    echo -e "\e[32mPass :)\e[0m"
+else
+    echo -e "\e[31mFail ;(\e[0m"
+fi
+
+# Test rectangle check
+echo -e -n "Testing non-rectangular maze . . . "
+./maze data/mazes/invalid/notRectangle.txt > output.tmp
+if grep -q "Error - Maze not rectangle";
+then
+    echo -e "\e[32mPass :)\e[0m"
+else
+    echo -e "\e[31mFail ;(\e[0m"
+fi
+
+# Test allowed chars check
+echo -e -n "Testing maze with illegals . . . "
+./maze data/mazes/invalid/invalChars.txt > output.tmp
+if grep -q "Error - Maze contains invalid characters";
+then
+    echo -e "\e[32mPass :)\e[0m"
+else
+    echo -e "\e[31mFail ;(\e[0m"
+fi
+
+# Test missing Start
+echo -e -n "Testing maze missing Start . . . "
+./maze data/mazes/invalid/noS.txt > output.tmp
+if grep -q "Error - Maze does not have Start Point";
+then
+    echo -e "\e[32mPass :)\e[0m"
+else
+    echo -e "\e[31mFail ;(\e[0m"
+fi
+
+# Test missing End
+echo -e -n "Testing maze missing End . . . "
+./maze data/mazes/invalid/noE.txt > output.tmp
+if grep -q "Error - Maze does not have End Point";
+then
+    echo -e "\e[32mPass :)\e[0m"
+else
+    echo -e "\e[31mFail ;(\e[0m"
+fi
+
+################################################################################
 
 echo -e "Map Display and Player Movement Tests"
 
 # Test Map Print
 echo -e -n "Testing map display . . . "
 ./maze data/mazes/valid/wasd-testing/wasd-tester.txt < data/inputs/WASD/map.in > output.tmp
-if grep cmp -s data/mazes/valid/wasd-testing/correct-m.txt output.tmp;
+if cmp -s data/mazes/valid/wasd-testing/correct-m.txt output.tmp;
 then
     echo -e "\e[32mPass :)\e[0m"
 else
@@ -61,7 +141,7 @@ fi
 # Test W
 echo -e -n "Testing player movement (W) . . . "
 ./maze data/mazes/valid/wasd-testing/wasd-tester.txt < data/inputs/WASD/W.in > output.tmp
-if grep cmp -s data/mazes/valid/wasd-testing/correct-w.txt output.tmp;
+if cmp -s data/mazes/valid/wasd-testing/correct-w.txt output.tmp;
 then
     echo -e "\e[32mPass :)\e[0m"
 else
@@ -71,7 +151,7 @@ fi
 # Test A
 echo -e -n "Testing player movement (A) . . . "
 ./maze data/mazes/valid/wasd-testing/wasd-tester.txt < data/inputs/WASD/A.in > output.tmp
-if grep cmp -s data/mazes/valid/wasd-testing/correct-a.txt output.tmp;
+if cmp -s data/mazes/valid/wasd-testing/correct-a.txt output.tmp;
 then
     echo -e "\e[32mPass :)\e[0m"
 else
@@ -81,7 +161,7 @@ fi
 # Test S
 echo -e -n "Testing player movement (S) . . . "
 ./maze data/mazes/valid/wasd-testing/wasd-tester.txt < data/inputs/WASD/S.in > output.tmp
-if grep cmp -s data/mazes/valid/wasd-testing/correct-s.txt output.tmp;
+if cmp -s data/mazes/valid/wasd-testing/correct-s.txt output.tmp;
 then
     echo -e "\e[32mPass :)\e[0m"
 else
@@ -91,13 +171,13 @@ fi
 # Test D
 echo -e -n "Testing player movement (D) . . . "
 ./maze data/mazes/valid/wasd-testing/wasd-tester.txt < data/inputs/WASD/D.in > output.tmp
-if grep cmp -s data/mazes/valid/wasd-testing/correct-d.txt output.tmp;
+if cmp -s data/mazes/valid/wasd-testing/correct-d.txt output.tmp;
 then
     echo -e "\e[32mPass :)\e[0m"
 else
     echo -e "\e[31mFail ;(\e[0m"
 fi
 
-
+################################################################################
 
 rm -f output.tmp
